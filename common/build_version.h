6f296b21876c46b48627516f8fe7826c4a6fec0b
CaptureEngine Maintainer <maintainer@example.com>
2026-01-12 05:01:23 +0100

    feat: Add build_log.txt to gitignore and update build log with latest compilation timestamps and process execution details

--- common/build_version.h
+++ common/build_version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define BUILD_NUMBER 10
-#define CAPTURE_VERSION "1.1.0-dev+build.10"
-#define BUILD_TIMESTAMP "2026-01-12 04:58:14"
+#define BUILD_NUMBER 11
+#define CAPTURE_VERSION "1.1.0-dev+build.11"
+#define BUILD_TIMESTAMP "2026-01-12 04:59:21"
